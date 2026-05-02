507c294f32b578c85da40e52765e8f3467de7335
Abraraw Bahiru <abrarawbahiru@gmail.com>
2026-05-02 08:49:37 +0300

    Create start.py
    Added my contact info and learning goals to make the profile more professional

---
+++ start.py
@@ -0,0 +1,12 @@
+# My first Python program on GitHub
+# Author: Abraraw Bahiru
+# Date: May 2026
+
+print("Hello GitHub!")
+print("My name is Abraraw Bahiru")
+print("I'm learning Python programming")
+
+# Simple math example
+x = 10
+y = 5
+print( x + y)
