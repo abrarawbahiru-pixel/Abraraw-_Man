ced610f1a63e18b7f75959711810aaa1cdc3c801
Abraraw Bahiru <abrarawbahiru@gmail.com>
2026-05-02 08:56:48 +0300

    Create about_me.py
     showing your name and goal

---
+++ about_me.py
@@ -0,0 +1,9 @@
+# About Abraraw Bahiru
+
+name = "Abraraw Bahiru"
+goal = "Become a Python developer"
+interests = ["Coding", "Learning", "Technology"]
+
+print(f"Name: {name}")
+print(f"Goal: {goal}")
+print(f"Interests: {interests}")
